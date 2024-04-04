20139dc128ae12fb0e95e98bf02fd421efe1035f
Creative280 <alejandro.taborda280@gmail.com>
2024-04-03 20:22:34 -0500

    Ejercicio8 resuelto

---
+++ ejercicio8/ejercicio8.js
@@ -0,0 +1,8 @@
+const edad = 20; 
+const ingresosMensuales = 1200; 
+
+if (edad >= 16 && ingresosMensuales >= 1000) {
+  console.log('Tienes que tributar.');
+} else {
+  console.log('No tienes que tributar.');
+}
