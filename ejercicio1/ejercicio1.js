8b95ac6fceb71c79993b6fe0ca8d0d70f585ce89
Alexander <alexanderacosta2286@gmail.com>
2024-04-03 19:41:01 -0500

    ejercicio 1

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,1 +1,10 @@
-console.log("ejercicio 1");
+function esPar(n) {
+    if (n % 2 == 0) {
+        return "El número es par.";
+    } else {
+        return "El número es impar.";
+    }
+}
+
+console.log(esPar(7));
+console.log(esPar(8));
